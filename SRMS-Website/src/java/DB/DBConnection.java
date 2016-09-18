e8a167469a35dd0f8f2d93888d1580de1ac49614
savinda <savindamaddd@gmail.com>
2016-09-18 14:48:26 +0530

    add db connection

--- SRMS-Website/src/java/DB/DBConnection.java
+++ SRMS-Website/src/java/DB/DBConnection.java
@@ -12,11 +12,14 @@ public class DBConnection {
         try {
             Class.forName("com.mysql.jdbc.Drver");
             Connection myConnection =DriverManager.getConnection("jdbc:mysql://localhost:3306/smart_rms", "root", "admin");
+            //Connection myConnection =DriverManager.getConnection("jdbc:mysql://br-cdbr-azure-south-b.cloudapp.net:3306/smart_rms","b50735a87d1621","8a720e5f");
+            //System.out.println("DB.DBConnection.connection()");
             return myConnection;
         } 
         catch (ClassNotFoundException | SQLException ex) 
         {
             Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+            //System.err.println(ex);
         }
         return null;
     }    
